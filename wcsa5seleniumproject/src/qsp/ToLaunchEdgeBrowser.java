398c56b98152090ea28e6a6ec3f7167006f84c43
Dhanraj <Dhanraj@Dhanraj>
2023-07-30 20:03:48 -0400

    commit

--- wcsa5seleniumproject/src/qsp/ToLaunchEdgeBrowser.java
+++ wcsa5seleniumproject/src/qsp/ToLaunchEdgeBrowser.java
@@ -1,14 +1,22 @@
 package qsp;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class ToLaunchEdgeBrowser {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.edge.driver","./drivers/msedgedriver.exe");
 		
-		new EdgeDriver(); //use to launch edge browser.
+	    WebDriver driver=new EdgeDriver(); //  use to launch Edge Browser.
+	    
+		// maximize the browser
+		 driver.manage().window().maximize();
+		  
+	    
+	    Thread.sleep(2000);
+	    driver.close();
 	}
 
 }
